192d1d0d3848385afb999a8481e34a12171769d0
idvrifat <idvrifat@gmail.com>
2023-06-09 17:42:55 +0600

    Project instruction added

--- concepts.py
+++ concepts.py
@@ -1,4 +1,30 @@
 """
+<---------------------Project Disscusstion------------------------>
+Banking Management System
+--------------------------------------------------
+Create a banking management system where-
+
+User
+
+Can create an account.
+Can deposit and withdrawal amount. 
+Can check available balance.
+Can transfer the amount from his account to another user account.
+Can check transaction history.
+Can take a loan from the bank twice of his total amount..
+
+Note - User can only withdraw and transfer money from his account if he has money in his account.
+If a user is unable to withdraw the amount of money he has deposited in the bank, he will get a message that the bank is bankrupt.
+
+Admin 
+
+Can create an account
+Can check the total available balance of the bank.
+Can check the total loan amount.
+Can on or off the loan feature of the bank.
+
+
+<========================================================================================================>
 
 Main Page Functions (methods) can be implement : 
 ---------------------------------------------------->
@@ -34,3 +60,4 @@ User methods that will implement :
 7. take_loan
 
 """
+                                                                      
