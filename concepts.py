9fae91738e16c507df2172c38f696f727e1ab48d
idvrifat <idvrifat@gmail.com>
2023-06-09 16:07:10 +0600

    first commit

---
+++ concepts.py
@@ -0,0 +1,36 @@
+"""
+
+Main Page Functions (methods) can be implement : 
+---------------------------------------------------->
+1. create_user
+2. login_user
+3. create_account
+4. deposit_amount
+5. withdraw_amount
+6. check_balance
+7. transfer_amount
+8. check_transaction_history
+9. take_loan
+10. check_total_balance
+11. check_total_loan_amount
+12. toggle_loan_feature
+13. run_banking_system
+
+Admin Page methods 
+------------------------------------>
+1. create_account
+2. check_total_balance
+3. check_total_loan_amount
+4. toggle_loan_feature
+
+User methods that will implement :
+------------------------------------------>
+1. create_account
+2. deposit_amount
+3. withdraw_amount
+4. check_balance
+5. transfer_amount
+6. check_transaction_history
+7. take_loan
+
+"""
